5a24af0c5f27b9157a283f899d3c0cf17ffaf23d
caiyaling <52552477@qq.com>
2026-04-17 09:15:45 +0800

    fix: 路由改为hash模式
    - 使用 createWebHashHistory 替代 createWebHistory
    - 路由路径格式变为 #/social 形式

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import SocialPlatform from '@/pages/NetworkWarfareResourse/AccountResource/SocialPlatform.vue'
 import InstantMessaging from '@/pages/NetworkWarfareResourse/AccountResource/InstantMessaging.vue'
 import BlogForum from '@/pages/NetworkWarfareResourse/AccountResource/BlogForum.vue'
@@ -98,7 +98,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/network-warfare-resource/'),
+  history: createWebHashHistory(),
   routes
 })
 
